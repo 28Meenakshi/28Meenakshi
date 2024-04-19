f5cb7006566925f35f75a2540c633ce2c64b1fe9
28Meenakshi <113116564+28Meenakshi@users.noreply.github.com>
2024-04-19 22:11:39 +0530

    Add files via upload

---
+++ ViewVacancy.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Vaccancy</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body style ="background-color:Skyblue";><center>
+    <h1>View Vaccancy </h1>
+    <table>
+        <tr class="row100 head">
+            <th>Designation</th>
+            <th>Salary</th>
+            <th>SSLC Minimum Percentage</th>
+            <th>Degree Minimum Percentage</th>
+            <th>Backlogs Allowed</th>
+            <th>Requirement</th>
+            <th>Interview Date</th>
+            <th>Company Details</th>
+            <th>Apply</th>
+        </tr>
+        <tr>
+            <td>Developer</td>
+            <td>20000</td>
+            <td>80</td>
+            <td>80</td>
+            <td>0</td>
+            <td>Software Skill</td>
+            <td>27/08/2024</td>
+            <td>101</td>
+            <td>Offline</td>
+        </tr>
+        <tr>
+            <td>Accounts</td>
+            <td>10000</td>
+            <td>65</td>
+            <td>70</td>
+            <td>0</td>
+            <td>Tally</td>
+            <td>20/08/2024</td>
+            <td>101</td>
+            <td>Offline</td>
+        </tr>
+    </table><center>
+</body>
+</html>
